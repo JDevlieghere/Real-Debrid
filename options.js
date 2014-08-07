5aa7bdfb1a0cbad16dc2161245f59260e1563180
JDevlieghere <info@jonasdevlieghere.com>
2014-08-07 23:14:44 +0200

    Added local dependencies. Added options

---
+++ options.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	chrome.storage.sync.get('user', function (result) {
+		$("input#username").val(result.user);
+	});
+	chrome.storage.sync.get('pass', function (result) {
+		$("input#password").val(result.pass);
+	});
+});
+
+$("button#save").click(function() {
+	var user = $("input#username").val();
+	chrome.storage.sync.set({'user': user});
+
+	var pass = $("input#password").val();
+	chrome.storage.sync.set({'pass': pass});
+	
+	return false;
+});

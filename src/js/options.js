a31caf1d0908b252667d4c8593bfb5e7ac31255e
JDevlieghere <info@jonasdevlieghere.com>
2014-08-17 19:47:21 +0200

    Working on options

---
+++ src/js/options.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    chrome.storage.sync.get('warningPercentage', function(result) {
+        $("input#warningPercentage").val(result.warningPercentage);
+    });
+
+    chrome.storage.sync.get('warningDays', function(result) {
+        $("input#warningDays").val(result.warningDays);
+    });
+});
+
+$("button#save").click(function() {
+    var warningPercentage = $("input#warningPercentage").val();
+    chrome.storage.sync.set({
+        'warningPercentage': warningPercentage
+    });
+
+    var warningDays = $("input#warningDays").val();
+    chrome.storage.sync.set({
+        'warningDays': warningDays
+    });
+
+    return false;
+});

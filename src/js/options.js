c5a61ebd34431265927019c813db6dd9cb30b17d
JDevlieghere <info@jonasdevlieghere.com>
2014-08-24 23:38:53 +0200

    added limited hosters

--- src/js/options.js
+++ src/js/options.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
     $('#version').html(version);
 
     getDownloads();
+    getAccount();
 });
 
 $("button#save").click(function() {
@@ -51,8 +52,7 @@ function failure() {
     });
 }
 
-function getDownloads() {
-    var url = "https://real-debrid.com/api/downloads.php?out=json";
+function api(url, callback) {
     $.ajax({
         type: "GET",
         url: url,
@@ -62,22 +62,56 @@ function getDownloads() {
         xhrFields: {
             withCredentials: true
         },
-        success: function(data) {
-            var downloads = data.downloads;
-            var html = "";
-            for (var i = 0; i < downloads.length; i++) {
-                html += '<tr>';
-                html += '<td><img src="' + downloads[i].hoster_image + '" //></td>';
-                html += '<td>' + downloads[i].name + '</td>';
-                html += '<td>' + downloads[i].generated_date + '</td>';
-                html += '<td><a href="' + downloads[i].original + '" target="_blank">Link</a></td>';
-                html += '<td><a href="' + downloads[i].link + '" target="_blank">Link</a></td>';
-                html += '</tr>';
-            }
-            $('#downloads tbody').append(html);
-        },
+        success: callback,
         error: function() {
             console.log("Could not reach real-debrid.com");
         }
+    });
+}
+
+function getAccount() {
+    var url = "https://real-debrid.com/api/account.php?out=json";
+    api(url, function(data) {
+        $('#username').html(data.username);
+        $('#email').html(data.email);
+        $('#points').html(data.points);
+        $('#expiration').html(data['expiration-txt']);
+        var html = '';
+        var limited = data.limited;
+        for (var i = 0; i < limited.length; i++) {
+            var hoster = limited[i];
+            var total = hoster.limit + hoster.additional_traffic;
+            var used = Math.round((hoster.downloaded / total) * 100);
+            html += '<div class="row">';
+            html += '<div class="key">' + hoster.name + '</div>';
+            html += '<div class="value">';
+            html += '<div class="progress">';
+            html += '<div class="progress-bar" role="progressbar" aria-valuenow="' + used + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + used + '%;">';
+            html += used + '%';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+        }
+        $('#hosters').append(html);
+    });
+
+}
+
+function getDownloads() {
+    var url = "https://real-debrid.com/api/downloads.php?out=json";
+    api(url, function(data) {
+        var downloads = data.downloads;
+        var html = '';
+        for (var i = 0; i < downloads.length; i++) {
+            html += '<tr>';
+            html += '<td><img src="' + downloads[i].hoster_image + '" //></td>';
+            html += '<td>' + downloads[i].name + '</td>';
+            html += '<td>' + downloads[i].generated_date + '</td>';
+            html += '<td><a href="' + downloads[i].original + '" target="_blank">Link</a></td>';
+            html += '<td><a href="' + downloads[i].link + '" target="_blank">Link</a></td>';
+            html += '</tr>';
+        }
+        $('#downloads tbody').append(html);
     });
 }

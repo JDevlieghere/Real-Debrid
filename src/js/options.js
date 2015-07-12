83ccc230990a0e85a260eae378e72024e0330b85
SublimePotato <max@pressburger.net>
2015-07-12 16:45:38 +0200

    additional check for "Downloads list disabled." to prevent hiding the account info.

--- src/js/options.js
+++ src/js/options.js
@@ -109,7 +109,11 @@ function api(url, callback) {
 }
 
 function loginRequired() {
-    $('section.login-required').html('You have to be logged in to use this feature.');
+    $('section.login-required.accountInfo').html('You have to be logged in to use this feature.');
+}
+
+function downloadListDisabled(){
+    $('section.login-required.latestDownloads').html('Your download list is disabled.');
 }
 
 function getAccount(callback) {
